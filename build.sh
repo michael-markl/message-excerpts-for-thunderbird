045c7a33d85ce81b1950d483e609be97f5fd1f43
Michael Markl <marklmichael98@gmail.com>
2026-08-04 23:20:21 +0200

    Add icon

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm -f addon.xpi
+
+for size in 16 32 48 64 128; do
+  rm -f icon-${size}.png
+  convert -background none -resize ${size}x${size} icon.svg icon-${size}.png || exit 1
+  optipng -o2 icon-${size}.png || exit 1
+done
+
+zip -r addon.xpi manifest.json background.js api/* icon*.png
